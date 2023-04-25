5c3a1f8b2f1c5bf9f2eb49ce20ec00242c50f670
rajinzajin <dorgclabs@gmail.com>
2023-04-25 23:06:59 +0800

    initial UI

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+				primary: "var(--color-primary)",
+				secondary: "var(--color-secondary)",
+				"higlight-1": "var(--color-highlight-1)",
+        "higlight-2": "var(--color-highlight-2)",
+				item: "rgb(45 50 90)",
+				money: "hsl(74.66666666666667, 100%, 60%)",
+				money2: "rgb(210 143 214)",
+				summary: "rgb(255 177 63)",
+			},
+      fontFamily: {
+				display: "Catamaran, sans-serif",
+				body: "Poppins, sans-serif",
+				market: "Chelsea Market, cursive",
+				material_icon: "Material Symbols Outlined"
+			},
+    },
+  },
+}
